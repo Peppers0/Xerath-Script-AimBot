d0d837ab19262d9cc830b607db10d17195a0513b
sajmonekk191 <82063567+sajmonekk191@users.noreply.github.com>
2021-07-29 19:21:07 +0200

    Add files via upload

---
+++ ScreenCapture/AppCAP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace AutoAIM.Scr
+{
+    class ApplicationCapture
+    {
+        public static Bitmap CaptureApplication(string procName)
+        {
+            var proc = Process.GetProcessesByName(procName)[0];
+            Interop.BringWindowToFront(procName);
+            var rect = new User32.Rect();
+            User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+            graphics.Dispose(); // not sure about this
+            return bmp;
+        }
+
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+        }
+    }
+}
---
+++ ScreenCapture/ScreenCapture.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using FastBitmapLib;
+
+namespace AutoAIM.Scr
+{
+    class ScreenCapture
+    {
+        private static Bitmap GetCapture()
+        {
+            return ApplicationCapture.CaptureApplication("League of Legends");
+        }
+        public static Point? GetColorPosition(Color color)
+        {
+            int searchValue = color.ToArgb();
+
+            using (Bitmap bmp = GetCapture())
+            {
+                using (FastBitmap bitmap = new FastBitmap(bmp))
+                {
+                    bitmap.Lock();
+
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        for (int y = 0; y < bmp.Height; y++)
+                        {
+                            if (searchValue == bitmap.GetPixelInt(x, y))
+                            {
+                                bitmap.Unlock();
+                                return new Point(x, y);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
